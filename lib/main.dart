bdf0510c1351719593a6267c46cd49f106425c45
kapil jhajhria <jhajhria44@gmail.com>
2019-10-11 20:59:04 +0530

    tic tac toe game complete

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'gamelogic.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -16,191 +17,34 @@ class TicTacToePage extends StatefulWidget {
 }
 
 class _TicTacToePageState extends State<TicTacToePage> {
-  Widget gameCube() {
-    return Container(
-      margin: EdgeInsets.all(10),
-      child: Column(
-        children: <Widget>[
-          Expanded(
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Expanded(
-                  child: OneBox(
-                    buttonChild: board[0][0],
-                    onPressed: () {
-                      if (legitMove(board[0][0])) {
-                        if (currentPlayer == 'X') {
-                          board[0][0] = xIcon;
-                        } else {
-                          board[0][0] = oIcon;
-                        }
-                      }
-                      if (xWinnerCheck(board) || oWinnerCheck(board)) {
-                        gameReset();
-                      } else {
-                        changePlayer(currentPlayer);
-                      }
-
-                      setState(() {});
-                    },
-                  ),
-                ),
-                Expanded(
-                  child: OneBox(
-                    buttonChild: board[0][1],
-                    onPressed: () {
-                      if (legitMove(board[0][1])) {
-                        if (currentPlayer == 'X') {
-                          board[0][1] = xIcon;
-                        } else {
-                          board[0][1] = oIcon;
-                        }
-                        changePlayer(currentPlayer);
-                      }
-                      setState(() {});
-                    },
-                  ),
-                ),
-                Expanded(
-                  child: OneBox(
-                    buttonChild: board[0][2],
-                    onPressed: () {
-                      if (legitMove(board[0][2])) {
-                        if (currentPlayer == 'X') {
-                          board[0][2] = xIcon;
-                        } else {
-                          board[0][2] = oIcon;
-                        }
-                        changePlayer(currentPlayer);
-                      }
-                      setState(() {});
-                    },
-                  ),
-                )
-              ],
-            ),
-          ),
-          Expanded(
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Expanded(
-                  child: OneBox(
-                    buttonChild: board[1][0],
-                    onPressed: () {
-                      if (legitMove(board[1][0])) {
-                        if (currentPlayer == 'X') {
-                          board[1][0] = xIcon;
-                        } else {
-                          board[1][0] = oIcon;
-                        }
-                        changePlayer(currentPlayer);
-                      }
-                      setState(() {});
-                    },
-                  ),
-                ),
-                Expanded(
-                  child: OneBox(
-                    buttonChild: board[1][1],
-                    onPressed: () {
-                      if (legitMove(board[1][1])) {
-                        if (currentPlayer == 'X') {
-                          board[1][1] = xIcon;
-                        } else {
-                          board[1][1] = oIcon;
-                        }
-                        changePlayer(currentPlayer);
-                      }
-                      setState(() {});
-                    },
-                  ),
-                ),
-                Expanded(
-                  child: OneBox(
-                    buttonChild: board[1][2],
-                    onPressed: () {
-                      if (legitMove(board[1][2])) {
-                        if (currentPlayer == 'X') {
-                          board[1][2] = xIcon;
-                        } else {
-                          board[1][2] = oIcon;
-                        }
-                        changePlayer(currentPlayer);
-                      }
-                      setState(() {});
-                    },
-                  ),
-                )
-              ],
-            ),
-          ),
-          Expanded(
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Expanded(
-                  child: OneBox(
-                    buttonChild: board[2][0],
-                    onPressed: () {
-                      if (legitMove(board[2][0])) {
-                        if (currentPlayer == 'X') {
-                          board[2][0] = xIcon;
-                        } else {
-                          board[2][0] = oIcon;
-                        }
-                        changePlayer(currentPlayer);
-                      }
-                      setState(() {});
-                    },
-                  ),
-                ),
-                Expanded(
-                  child: OneBox(
-                    buttonChild: board[2][1],
-                    onPressed: () {
-                      if (legitMove(board[2][1])) {
-                        if (currentPlayer == 'X') {
-                          board[2][1] = xIcon;
-                        } else {
-                          board[2][1] = oIcon;
-                        }
-                        changePlayer(currentPlayer);
-                      }
-                      setState(() {});
-                    },
-                  ),
-                ),
-                Expanded(
-                  child: OneBox(
-                    buttonChild: board[2][2],
-                    onPressed: () {
-                      if (legitMove(board[2][2])) {
-                        if (currentPlayer == 'X') {
-                          board[2][2] = xIcon;
-                        } else {
-                          board[2][2] = oIcon;
-                        }
-                        changePlayer(currentPlayer);
-                      }
-                      setState(() {});
-                    },
-                  ),
-                )
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
+    void winnerPopup() {
+      if (xWinnerCheck(board) || oWinnerCheck(board)) {
+        Alert(
+          context: context,
+          title: "game Over",
+          desc: "Player $currentPlayer won",
+          buttons: [
+            DialogButton(
+              child: Text(
+                "Restart",
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+              onPressed: () {
+                gameReset();
+                setState(() {});
+                return Navigator.pop(context);
+              },
+              width: 120,
+            )
+          ],
+        ).show();
+      } else {
+        changePlayer(currentPlayer);
+      }
+    }
+
     return Scaffold(
       backgroundColor: Color(0xFFD6AA7C),
       body: SafeArea(
@@ -242,7 +86,182 @@ class _TicTacToePageState extends State<TicTacToePage> {
             ),
             Expanded(
               flex: 7,
-              child: gameCube(),
+              child: Container(
+                margin: EdgeInsets.all(10),
+                child: Column(
+                  children: <Widget>[
+                    Expanded(
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          Expanded(
+                            child: OneBox(
+                              buttonChild: board[0][0],
+                              onPressed: () {
+                                if (legitMove(board[0][0])) {
+                                  if (currentPlayer == 'X') {
+                                    board[0][0] = xIcon;
+                                  } else {
+                                    board[0][0] = oIcon;
+                                  }
+                                  winnerPopup();
+                                }
+
+                                setState(() {});
+                              },
+                            ),
+                          ),
+                          Expanded(
+                            child: OneBox(
+                              buttonChild: board[0][1],
+                              onPressed: () {
+                                if (legitMove(board[0][1])) {
+                                  if (currentPlayer == 'X') {
+                                    board[0][1] = xIcon;
+                                  } else {
+                                    board[0][1] = oIcon;
+                                  }
+                                  winnerPopup();
+                                }
+                                setState(() {});
+                              },
+                            ),
+                          ),
+                          Expanded(
+                            child: OneBox(
+                              buttonChild: board[0][2],
+                              onPressed: () {
+                                if (legitMove(board[0][2])) {
+                                  if (currentPlayer == 'X') {
+                                    board[0][2] = xIcon;
+                                  } else {
+                                    board[0][2] = oIcon;
+                                  }
+                                  winnerPopup();
+                                }
+                                setState(() {});
+                              },
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          Expanded(
+                            child: OneBox(
+                              buttonChild: board[1][0],
+                              onPressed: () {
+                                if (legitMove(board[1][0])) {
+                                  if (currentPlayer == 'X') {
+                                    board[1][0] = xIcon;
+                                  } else {
+                                    board[1][0] = oIcon;
+                                  }
+                                  winnerPopup();
+                                }
+                                setState(() {});
+                              },
+                            ),
+                          ),
+                          Expanded(
+                            child: OneBox(
+                              buttonChild: board[1][1],
+                              onPressed: () {
+                                if (legitMove(board[1][1])) {
+                                  if (currentPlayer == 'X') {
+                                    board[1][1] = xIcon;
+                                  } else {
+                                    board[1][1] = oIcon;
+                                  }
+                                  winnerPopup();
+                                }
+                                setState(() {});
+                              },
+                            ),
+                          ),
+                          Expanded(
+                            child: OneBox(
+                              buttonChild: board[1][2],
+                              onPressed: () {
+                                if (legitMove(board[1][2])) {
+                                  if (currentPlayer == 'X') {
+                                    board[1][2] = xIcon;
+                                  } else {
+                                    board[1][2] = oIcon;
+                                  }
+                                  winnerPopup();
+                                }
+                                setState(() {});
+                              },
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          Expanded(
+                            child: OneBox(
+                              buttonChild: board[2][0],
+                              onPressed: () {
+                                if (legitMove(board[2][0])) {
+                                  if (currentPlayer == 'X') {
+                                    board[2][0] = xIcon;
+                                  } else {
+                                    board[2][0] = oIcon;
+                                  }
+                                  winnerPopup();
+                                }
+                                setState(() {});
+                              },
+                            ),
+                          ),
+                          Expanded(
+                            child: OneBox(
+                              buttonChild: board[2][1],
+                              onPressed: () {
+                                if (legitMove(board[2][1])) {
+                                  if (currentPlayer == 'X') {
+                                    board[2][1] = xIcon;
+                                  } else {
+                                    board[2][1] = oIcon;
+                                  }
+                                  winnerPopup();
+                                }
+                                setState(() {});
+                              },
+                            ),
+                          ),
+                          Expanded(
+                            child: OneBox(
+                              buttonChild: board[2][2],
+                              onPressed: () {
+                                if (legitMove(board[2][2])) {
+                                  if (currentPlayer == 'X') {
+                                    board[2][2] = xIcon;
+                                  } else {
+                                    board[2][2] = oIcon;
+                                  }
+                                  winnerPopup();
+                                }
+                                setState(() {});
+                              },
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
             Expanded(
               flex: 2,
