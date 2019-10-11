7fe153c20a3f0cda93288c49ace8a8a5ab060239
kapil jhajhria <jhajhria44@gmail.com>
2019-10-11 19:18:10 +0530

    imported font awesome
    source
    https://pub.dev/packages/font_awesome_flutter#-installing-tab-

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() {
   runApp(MaterialApp(
