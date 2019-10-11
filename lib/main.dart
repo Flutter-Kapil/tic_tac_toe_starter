4ee7b1117727d70d99ed7f7a2c81258ce4235e5d
kapil jhajhria <jhajhria44@gmail.com>
2019-10-11 20:59:32 +0530

    removed not used import

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'gamelogic.dart';
 import 'package:rflutter_alert/rflutter_alert.dart';
 
